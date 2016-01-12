edf46f42e7ed4859978bacf477fb0637d9439cf0
lagage <flagage@adents.com>
2016-01-12 17:49:46 +0100

    premier commit partie 3

---
+++ e-bibliotheque/e-bibliotheque/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_bibliotheque.Startup))]
+namespace e_bibliotheque
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
